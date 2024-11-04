0ab1b1d9817088f5552a53369b7ead6d2e148edc
mark1382 <66511007+mark1382@users.noreply.github.com>
2024-11-04 10:39:45 +0100

    Update zad1.cpp

--- zad1.cpp
+++ zad1.cpp
@@ -1,13 +1,43 @@
-#include "catch.hpp"
+// ResourceManager.hpp
 
-#include "ResourceManager.hpp"
+#include <memory>
+#include "Resource.hpp"
 
-TEST_CASE("RAII", "[]")
-{
-    {
-        ResourceManager rm{};
-        REQUIRE(ConstructionTracker::defaults == 1);
-        REQUIRE_NOTHROW(rm.get());
+class ResourceManager {
+private:
+    std::unique_ptr<Resource> resource; // Inteligentny wskaźnik zarządzający zasobem
+
+public:
+    // Konstruktor domyślny
+    ResourceManager() : resource(std::make_unique<Resource>()) {}
+
+    // Kopiujący konstruktor
+    ResourceManager(const ResourceManager& other) : resource(std::make_unique<Resource>(*other.resource)) {}
+
+    // Przenoszący konstruktor
+    ResourceManager(ResourceManager&& other) noexcept : resource(std::move(other.resource)) {}
+
+    // Operator przypisania kopiujący
+    ResourceManager& operator=(const ResourceManager& other) {
+        if (this != &other) {
+            resource = std::make_unique<Resource>(*other.resource);
+        }
+        return *this;
     }
-    REQUIRE(ConstructionTracker::live == 0);
-}
+
+    // Operator przypisania przenoszący
+    ResourceManager& operator=(ResourceManager&& other) noexcept {
+        if (this != &other) {
+            resource = std::move(other.resource);
+        }
+        return *this;
+    }
+
+    // Destruktor
+    ~ResourceManager() = default;
+
+    // Metoda get, która zwraca wynik metody get() klasy Resource
+    double get() const {
+        return resource->get();
+    }
+};
